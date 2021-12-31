1d96c4fc81315284be66c2a6167e6af575e84978
Pranav Pandey <pranavpandey@Pranavs-MacBook-Air.local>
2022-01-01 00:28:56 +0530

    Server added

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import adminRoutes from "./routes/admin.js";
+// import teacherRoutes from "./routes/teacher.js";
+// import studentRoutes from "./routes/student.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/admin", adminRoutes);
+// app.use("/teacher", teacherRoutes);
+// app.use("/student", studentRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("Page not found");
+});
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false,
+  })
+  .then((result) => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running at ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
