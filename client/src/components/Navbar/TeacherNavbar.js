5697fb38a6a7d029442fcc12d0e8700d6c20ffcc
Pranav Pandey <pranavpandey@Pranavs-Air.domain.name>
2022-01-04 19:59:47 +0530

    Teacher Navbar added

--- client/src/components/Navbar/TeacherNavbar.js
+++ client/src/components/Navbar/TeacherNavbar.js
@@ -1,32 +1,60 @@
-import React, { Fragment } from "react";
+import { Fragment } from "react";
 import { Button } from "@material-ui/core";
-import { NavLink } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import useStyles from "./styles";
 
 const TeacherNavbar = () => {
   const classes = useStyles();
-  const data = useSelector((state) => state.user);
+  const location = useLocation();
+  const path = location.pathname;
+  const user = useSelector((state) => state.profile.user);
 
   return (
     <Fragment>
       <Button
-        className={classes.button}
-        activeClassName={classes.active}
-        exact
-        component={NavLink}
-        to={`/teacher/home/${data.user.registrationID}`}
+        className={
+          path === `/teacher/home/${user.registrationID}`
+            ? classes.active
+            : classes.button
+        }
+        component={Link}
+        to={`/teacher/home/${user.registrationID}`}
       >
         Home
       </Button>
       <Button
-        className={classes.button}
-        activeClassName={classes.active}
-        exact
-        component={NavLink}
-        to="/teacher/mark-attendance"
+        className={
+          path === `/teacher/mark-attendance/${user.registrationID}`
+            ? classes.active
+            : classes.button
+        }
+        component={Link}
+        to={`/teacher/mark-attendance/${user.registrationID}`}
       >
         Mark Attendance
+      </Button>
+      <Button
+        className={
+          path === `/teacher/give-marks/${user.registrationID}`
+            ? classes.active
+            : classes.button
+        }
+        component={Link}
+        to={`/teacher/give-marks/${user.registrationID}`}
+      >
+        Give Marks
+      </Button>
+      <Button
+        className={
+          path === `/teacher/update-mark/${user.registrationID}`
+            ? classes.active
+            : classes.button
+        }
+        component={Link}
+        to={`/teacher/update-mark/${user.registrationID}`}
+      >
+        Update Mark
       </Button>
     </Fragment>
   );
