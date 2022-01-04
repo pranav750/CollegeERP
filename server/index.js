a3dc9a2a7e2cf00f4ebd92bab15f38d615ac6b20
Pranav Pandey <pranavpandey@Pranavs-Air.domain.name>
2022-01-04 20:06:38 +0530

    Server index file updated

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import adminRoutes from "./routes/admin.js";
-// import teacherRoutes from "./routes/teacher.js";
-// import studentRoutes from "./routes/student.js";
+import studentRoutes from "./routes/student.js";
+import teacherRoutes from "./routes/teacher.js";
 
 dotenv.config();
 
@@ -15,8 +15,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/admin", adminRoutes);
-// app.use("/teacher", teacherRoutes);
-// app.use("/student", studentRoutes);
+app.use("/student", studentRoutes);
+app.use("/teacher", teacherRoutes);
 
 app.use((req, res, next) => {
   res.status(404).send("Page not found");
